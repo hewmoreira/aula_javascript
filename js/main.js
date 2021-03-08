270df688894971ed840a2fc7cf490f1306984c74
Hewerton Moreira <79805868+hewmoreira@users.noreply.github.com>
2021-03-08 20:14:13 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,113 @@
+
+function clicou(){
+        document.getElementById("agradecimento").innerHTML = "Obrigado por Clicar";
+
+//    console.log(document.getElementById("agradecimento"));
+//    alert("Obrigado por clicar");
+}
+/*
+function redirecionar(){
+    window.open("http://www.g1.com.br");
+    window.location.href = "http://www.g1.com.br";
+}
+
+function trocar(elemento){
+ //   document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+  //  alert("trocar texto");
+  elemento.innerHTML = "Obrigado por passar o mouse";
+}
+
+function voltar(elemento){
+//    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("pagina carregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
+*/
+/*
+function soma(n1, n2) { //função algébrica
+    return n1 + n2; //retorno da soma
+}
+
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    }else {validar = false
+    }
+    return validar;
+}
+
+var idade = prompt ("Qual sua idade?");
+console.log (validaIdade(idade));
+*/
+
+
+/*  function setReplace (frase, nome, novo_nome){ //função para replace do que foi setado
+    return frase.replace(nome, novo_nome) //palavras que vão ser substituídas
+} */
+
+
+
+/*  alert(soma(5, 10)); //retorna a soma da function 
+    alert(setReplace("Vai Japão", "Japão", "Brasil")); //retorna a frase com a palavra setada pro Replace
+*/
+
+/*
+
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+
+var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+}
+
+var count = 0;
+while (count<=5){
+    console.log(count);
+    alert(count);
+    count = count + 1;
+}
+
+var idade = prompt("Qual sua idade?")
+if (idade>=18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+
+var nome = "Hewerton Moreira";
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo!"
+alert(nome + " tem " + idade + " anos");
+alert (idade + idade2);
+console.log (nome);
+console.log (n1*n2);
+console.log (frase.toLowerCase);
+alert(frase.replace("Japão","Brasil"));
+
+var lista = ["maçã","pêra","laranja"];
+lista.push ("uva");
+lista.pop ();
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" - "))
+
+var fruta = {nome: "maça", cor: "vermelha"};
+console.log(fruta.nome);
+
+var fruta = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}];
+console.log(fruta);
+alert(fruta[1].nome);
+
+*/
